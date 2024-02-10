5b3cee75122fd6f3ab13151e7574348a0f242c25
Ula <ursz.kam@gmail.com>
2024-02-10 19:21:00 +0100

    initial commit

---
+++ get_next_line.c
@@ -0,0 +1,13 @@
+/*
+Repeated calls to get_next_line should enable sequential reading 
+of the text file pointed to by the file descriptor, 
+processing one line at a time. The function returns the read line
+or NULL if there is nothing else to read or if an error occurs.
+
+Ensure that the function works seamlessly for both file reading and standard input.
+*/
+
+char	*get_next_line(int fd)
+{
+
+}
