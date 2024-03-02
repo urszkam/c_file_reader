cfea4c9f1f65d2769802d8450f917023b0aea3e0
Ula <66919420+urszkam@users.noreply.github.com>
2024-03-02 23:06:53 +0100

    update prototypes

--- get_next_line.h
+++ get_next_line.h
@@ -4,6 +4,9 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+int		ft_strlen(char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strndup(char *str, int n);
 char	*get_next_line(int fd);
 
 #endif
