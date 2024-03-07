172338724ffa973170c817d65cf15e559c214580
Ula <66919420+urszkam@users.noreply.github.com>
2024-03-07 22:33:20 +0100

    fix const prototypes

--- get_next_line.h
+++ get_next_line.h
@@ -16,9 +16,9 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-size_t		ft_strlen(char *str);
+size_t		ft_strlen(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strndup(char *str, int n);
+char	*ft_strdup(char *str);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strchr(const char *s, int c);
 char	*get_next_line(int fd);
